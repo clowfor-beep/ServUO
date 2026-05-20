e21ecaa2819b110903ab631e72f2503cae26e3ac
Jacob <clowfor@gmail.com>
2026-05-20 16:50:32 +0200

    Fix start.sh: remove -noconsole so screen can send worldsave commands

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ cd /home/servuo
 # Start server in a detached screen session
 # Use direct file redirection to avoid pipe buffer deadlocks
 screen -dmS servuo bash -c \
-  "mono ServUO.exe -noconsole < /dev/null >> /home/servuo/servuo.log 2>&1"
+  "mono ServUO.exe >> /home/servuo/servuo.log 2>&1"
 
 echo "ServUO started in screen session 'servuo'"
 echo "Attach with: screen -r servuo"
