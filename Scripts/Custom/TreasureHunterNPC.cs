e63e063ef45a2c71df4500544e3b877679e2e600
Louise Thostrup <selitora@gmail.com>
2026-06-02 21:29:15 +0200

    TreasureHunterNPC: set Cartography 100; BulkResourceMerchant: sell deeds; fix TravelingRareMerchant null bytes

--- Scripts/Custom/TreasureHunterNPC.cs
+++ Scripts/Custom/TreasureHunterNPC.cs
@@ -143,6 +143,8 @@ namespace Server.Custom
             SetStr(100); SetDex(100); SetInt(100);
             SetHits(200);
 
+            SetSkill(SkillName.Cartography, 100.0, 100.0);
+
             // Weathered explorer / treasure hunter outfit
             int leather = Utility.RandomList(1109, 2117, 2213, 2306); // earthy leather browns
             int boot    = Utility.RandomList(1107, 2101, 2306);       // dark boot tones
